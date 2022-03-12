5836245a65c68b6ce7e469e96af2eca2a66e0dae
miillz <romaric.miaka.sio@gmail.com>
2022-03-12 22:45:28 +0100

    config User Identity

---
+++ App_Start/AppUserStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using portail.Models;
+
+
+public class AppUserStore: UserStore<User>
+{
+    public AppUserStore(PortailContext context) : base(context)
+    {
+    }
+}
